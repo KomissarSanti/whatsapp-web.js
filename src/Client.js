122c4f302adf76bb9e7882bb1e469fb4f5c8fc17
Илья <ilichs.vl@gmail.com>
2025-09-01 22:12:49 +1000

    2.33 update

--- src/Client.js
+++ src/Client.js
@@ -263,10 +263,18 @@ class Client extends EventEmitter {
             await this.pupPage.waitForNavigation({waitUntil: 'load', timeout: 5000}).catch((_) => _);
         });
         await this.pupPage.evaluate(() => {
+            if (window.AuthStore.AppState.hasSynced) {
+                window.onAppStateHasSyncedEvent();
+            }
+
             window.AuthStore.AppState.on('change:state', (_AppState, state) => { window.onAuthAppStateChangedEvent(state); });
-            window.AuthStore.AppState.on('change:hasSynced', () => { window.onAppStateHasSyncedEvent(); });
+            window.AuthStore.AppState.on('change:hasSynced', (_AppState, state) => {
+                if (state !== window.AuthStore.AppState.hasSynced) {
+                    window.onAppStateHasSyncedEvent();
+                }
+            });
             window.AuthStore.Cmd.on('offline_progress_update', () => {
-                window.onOfflineProgressUpdateEvent(window.AuthStore.OfflineMessageHandler.getOfflineDeliveryProgress()); 
+                window.onOfflineProgressUpdateEvent(window.AuthStore.OfflineMessageHandler.getOfflineDeliveryProgress());
             });
             window.AuthStore.Cmd.on('logout', async () => {
                 await window.onLogoutEvent();
