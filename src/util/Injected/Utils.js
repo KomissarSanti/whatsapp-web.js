2446019d2d1ee56b0d138cf45c9512163aef759a
Илья <ilichs.vl@gmail.com>
2025-10-01 18:02:38 +1000

    fix send message

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -539,30 +539,30 @@ exports.LoadUtils = () => {
                 chat = null;
             }
         } else {
-            // chat = window.Store.Chat.get(chatWid) || (await window.Store.Chat.find(chatWid));
-            let isGroup = /@g.us/.test(chatId);
+            chat = window.Store.Chat.get(chatWid) || (await window.Store.Chat.find(chatWid));
+            // let isGroup = /@g.us/.test(chatId);
 
-            if (isGroup) {
-                chat = (await window.Store.FindOrCreateChat(chatWid)).chat;
-                // console.log('isGro', chatWid, isGroup, chat);
-            }
-            else {
-                // chat = await window.Store.FindOrCreateChat(chatWid)
-                // .then(chat => chat.chat)
-                // .catch(async err => {
-
-                let actions = [{
-                    type: "add",
-                    phoneNumber: chatWid.user
-                }]
-                let query = window.require("WAWebContactSyncUtils").constructUsyncDeltaQuery(actions);
-                let result =  await query.execute();
-                let lid = window.Store.WidFactory.createWid(result.list[0].lid);
-                chat = (await window.Store.FindOrCreateChat(lid, 'username_contactless_search')).chat;
-
-                // console.log('GET CHAT', chatWid, lid, chat);
-                // })
-            }
+            // if (isGroup) {
+            //     chat = (await window.Store.FindOrCreateChat(chatWid)).chat;
+            //     // console.log('isGro', chatWid, isGroup, chat);
+            // }
+            // else {
+            //     // chat = await window.Store.FindOrCreateChat(chatWid)
+            //     // .then(chat => chat.chat)
+            //     // .catch(async err => {
+            //
+            //     let actions = [{
+            //         type: "add",
+            //         phoneNumber: chatWid.user
+            //     }]
+            //     let query = window.require("WAWebContactSyncUtils").constructUsyncDeltaQuery(actions);
+            //     let result =  await query.execute();
+            //     let lid = window.Store.WidFactory.createWid(result.list[0].lid);
+            //     chat = (await window.Store.FindOrCreateChat(lid, 'username_contactless_search')).chat;
+            //
+            //     // console.log('GET CHAT', chatWid, lid, chat);
+            //     // })
+            // }
         }
 
         return getAsModel && chat
