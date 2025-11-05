6bcfcd5009cefc433d20edc2a775aed18e1bcca9
Илья <ilichs.vl@gmail.com>
2025-11-05 23:05:09 +1000

    fix mess id

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -237,7 +237,7 @@ exports.LoadUtils = () => {
 
         const lidUser = window.Store.User.getMaybeMeLidUser();
         const meUser = window.Store.User.getMaybeMePnUser();
-        const newId = await window.Store.MsgKey.newId();
+        const newId = (options.messageId ? options.messageId : await window.Store.MsgKey.newId());
         let from = chat.id.isLid() ? lidUser : meUser;
         let participant;
 
